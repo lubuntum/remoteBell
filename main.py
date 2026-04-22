79bc72cb1e22baf585d6d3eb205bd960ee0e98da
lubuntum <lubuntum@gmail.com>
2026-04-22 15:05:01 +0700

    Добавил апи для добавления событий + настроил логгер и подключил таску на проигрывание треков из сервиса

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-import platform
-import subprocess
+import asyncio
+from contextlib import asynccontextmanager
+import logging
+from pathlib import Path
 
 import pygame
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import os
 import json
 from models.requests.AudioRequest import AudioRequest
-from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from models.requests.Event import Event
+from services.ScheduleService import ScheduleService, SCHEDULES_PATH, AUDIO_DIR
+
+scheduleService = ScheduleService(SCHEDULES_PATH, AUDIO_DIR)
+
+LOG_DIR = Path("logs")
+LOG_DIR.mkdir(exist_ok=True)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_DIR/"schedule_service.log", encoding="UTF-8"),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Load schedule service...")
+    task = asyncio.create_task(scheduleService.run())
+    logger.info("task is running")
+    yield
+    task.cancel()
+    pygame.mixer.music.stop()
+    logger.info("Schedule service stopped")
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="templates", html=True), name="static")
 app.mount("/audio", StaticFiles(directory="audio"))
@@ -20,6 +48,7 @@ pygame.mixer.init()
 current_playing = None
 is_playing = False
 schedules_path = "schedules/schedules.json"
+
 
 @app.get("/")
 async def root():
@@ -64,6 +93,7 @@ async def play_audio(request: AudioRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error playing: {str(e)}")
 
+
 @app.post("/api/stop-audio")
 async def stop_audio():
     global is_playing
@@ -73,15 +103,19 @@ async def stop_audio():
         "status": "stopped",
         "message": "Аудио остановлено"
     }
+
+
 @app.get("/api/schedules")
 async def get_schedules():
-    with open(schedules_path, 'r', encoding = "UTF-8") as file:
+    with open(schedules_path, 'r', encoding="UTF-8") as file:
         data = json.load(file)
     return data
+
+
 @app.delete("/api/schedules/days/events")
 async def remove_event(day: str, time: str):
     with open(schedules_path, 'r', encoding="UTF-8") as file:
-        data = json.load(file) 
+        data = json.load(file)
 
     for schedule in data["schedules"]:
         if schedule["day"] == day:
@@ -89,9 +123,37 @@ async def remove_event(day: str, time: str):
             break
     else:
         return {"error": "Day not found"}, 404
-    
+
     with open(schedules_path, 'w', encoding="UTF-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
-    
+
     return {"message": f"Event at {time} on {day} removed"}
-    
+
+
+@app.post("/api/schedules/days/events")
+async def add_event(event: Event):
+    day = event.day
+    time = event.time
+    filepath = event.filepath
+    duration = event.duration
+    with open(schedules_path, 'r', encoding="UTF-8") as file:
+        data = json.load(file)
+    if not all([day, time, filepath, duration]):
+        return {"error", "not all fields present"}, 400
+    for schedule in data["schedules"]:
+        if schedule["day"] == day:
+            for existing_event in schedule["events"]:
+                if existing_event["time"] == time:
+                    logger.error(f"event at time {time} already exists")
+                    return {"error", f"event at time {time} already exists"}, 409
+            schedule["events"].append(
+                {"time": time,
+                 "filepath": filepath,
+                 "duration": duration})
+            break
+    else:
+        logger.error("Day not found")
+        return {"error": "Day not found"}, 404
+    with open(schedules_path, 'w', encoding="UTF-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+    return {"message": f"Event at {time} on {day} added"}
